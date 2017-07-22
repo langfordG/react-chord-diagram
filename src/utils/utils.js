c318a49b1862094259bafc24584bb0b01a9519d7
Grayson Langford <graysonlangford@Graysons-MacBook-Pro.local>
2017-07-22 10:55:07 -0700

    Remove groupTicks function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,13 +1,3 @@
-import { range } from 'd3-array';
-
-/* Returns an array of tick angles and values for a given group and step. See: https://bl.ocks.org/mbostock/4062006 */
-export const groupTicks = (group, step) => {
-
-    let k = (group.endAngle - group.startAngle) / group.value;
-
-    return range(0, group.value, step).map((value) => ({value, angle: value * k + group.startAngle}));
-};
-
 /* Determines what ribbons should be hidden while mousing over a group */
 export const isHiddenRibbon = (mouseOverGroup, sourceIndex, targetIndex) => {
 
